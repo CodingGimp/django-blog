f2b605b48fa4d8c51ebf2fe36eab304c8e08c3f3
CodingGimp <coding.gimp@gmail.com>
2019-07-01 12:24:39 -0700

    Setting up templates

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,27 @@
 from django.shortcuts import render
-from django.http import HttpResponse 
+from django.http import HttpResponse
+
+posts = [
+    {
+        'author': 'Mario Gimp',
+        'title': 'First post',
+        'content': 'Content of the first post',
+        'date_posted': 'July 1, 2019'
+    },
+    {
+        'author': 'Imran Majid',
+        'title': 'Second post',
+        'content': 'Content of the second post',
+        'date_posted': 'July 2, 2019'
+    }
+]    
 
 # Create views here.
 def home(request):
-    return render(request, 'blog/home.html')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
-    return HttpResponse('<h1>About Me</h1>')
+    return render(request, 'blog/about.html')
