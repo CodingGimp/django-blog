eab369c55ea881d352d51e1c7017c95e4fe41d51
CodingGimp <coding.gimp@gmail.com>
2019-07-01 17:08:43 -0700

    Database

--- blog/views.py
+++ blog/views.py
@@ -1,25 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Post
 
-posts = [
-    {
-        'author': 'Mario Gimp',
-        'title': 'First post',
-        'content': 'Content of the first post',
-        'date_posted': 'July 1, 2019'
-    },
-    {
-        'author': 'Imran Majid',
-        'title': 'Second post',
-        'content': 'Content of the second post',
-        'date_posted': 'July 2, 2019'
-    }
-]    
-
-# Create views here.
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
